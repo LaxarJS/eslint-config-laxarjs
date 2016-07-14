ab3fc16d82fb9650b88317cc5fb49887f82eb4eb
Jonas Pommerening <jonas.pommerening@gmail.com>
2016-07-14 16:34:43 +0200

    Add rules for node v4

---
+++ node.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2016 aixigo AG
+ * Released under the MIT license.
+ * http://laxarjs.org/license
+ */
+/* eslint-env node */
+
+module.exports = {
+   extends: require.resolve( './index' ),
+   env: {
+      browser: false,
+      node: true
+   },
+   rules: {
+      // not supported by node v4:
+      'prefer-rest-params': 'off',
+      'prefer-spread': 'off',
+      'prefer-template': 'off'
+   }
+};
